5ca8cba04405671ebfb62925ac322247d06e38cd
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2023-04-07 08:02:12 +0300

    1

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// Source: https://image.freepik.com/free-vector/happy-unicorn-background-walking-rainbow-flat-design_23-2147658728.jpg
